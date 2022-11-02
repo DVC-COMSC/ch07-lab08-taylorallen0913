ac346a8c6ec0e97de08fe3acad4a446d3d0ea6c1
Taylor Allen <taylorallen0913@gmail.com>
2022-11-01 17:35:32 -0700

    done

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-# ******************************
-# Make your Code
-# ******************************
+numbers = list(map(int, input().split()))
+number = int(input())
+
+index_to_insert = -1
+
+for i in range(1, len(numbers)):
+    if number < numbers[0]:
+        numbers.insert(0, number)
+        break
+    if number > numbers[len(numbers) - 1]:
+        numbers.insert(len(numbers), number)
+        break
+    if number < numbers[i]:
+        numbers.insert(i, number)
+        break
+
+print(numbers)
